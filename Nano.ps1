20685e3dcd84d2e1ac66802044ff054722f9ec83
Pavel Sushko <76505108+Pavel-Sushko@users.noreply.github.com>
2021-01-11 21:02:21 -0500

    Changed to open notepad++ instead of notepad, and fixing the issue of not being able to create/open files from other folder

--- Nano.ps1
+++ Nano.ps1
@@ -1,23 +1,23 @@
 # Nano from linux
-function nano {
-Param ([string]$f)
-if ($f = "$f.txt") {
-    $e = [io.path]::GetFileNameWithoutExtension(".\$f")
-    $f = $e
-    if (Test-Path $f -PathType leaf){
-    start notepad++ $f
+function nano
+{
+    param ([string][Parameter(ValueFromPipeline=$true)] $Name)
+    
+    if (Test-Path $Name)
+    {
+        start notepad++ $Name
+
+        $check = $true
+
+        ls $Name
     }
-    else {
-    New-Item .\$f
-    start notepad++ $f}
+    else
+    {
+        echo $check
+        New-Item ".\$Name"
+
+        start notepad++ $Name
+
+        ls $Name
     }
-else{
-    if (Test-Path "$f.txt" -PathType leaf){
-    start notepad "$f.txt"
-    }
-    else {
-    New-Item ".\$f.txt"
-    start notepad "$f.txt"}
-    }
-ls $f*
 }
